9d4d790226f49d9cb5539254b34498bae7b7dfc8
Matt Glowacki <mattwglowacki@gmail.com>
2023-08-27 20:28:41 -0400

    Complete personal info form

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,3 +1,11 @@
-export default function PersonalInfo() {
-
+/* eslint-disable react/prop-types */
+export default function PersonalInfo({ personalInfo }) {
+  return (
+    <>
+      <div>{personalInfo.firstName + ' ' + personalInfo.lastName} </div>
+      <div>{personalInfo.address}</div>
+      <div>{personalInfo.email}</div>
+      <div>{personalInfo.phone}</div>
+    </>
+  )
 }
--- src/components/PersonalInfoForm.jsx
+++ src/components/PersonalInfoForm.jsx
@@ -4,20 +4,24 @@ export default function PersonalInfoForm({ personalInfo, setPersonalInfo }) {
   function handleChange(e){
     e.preventDefault()
     console.log(e)
-    setPersonalInfo(e.target.value) // 
+    setPersonalInfo({ ...personalInfo, [e.target.id]: e.target.value } ) // 
   }
 
   return (
     <div className="personalForm">
-      <InputField id="firstName" value={personalInfo} onChange={handleChange} />
+      <InputField id="firstName" label="First Name" value={personalInfo.firstName} onChange={handleChange} />
+      <InputField id="lastName" label="Last Name" value={personalInfo.lastName} onChange={handleChange} />
+      <InputField id="address" label="Address" value={personalInfo.address} onChange={handleChange} />
+      <InputField id="email" label="E-mail" value={personalInfo.email} onChange={handleChange} />
+      <InputField id="phone" label="Phone Number" value={personalInfo.phone} onChange={handleChange} />
     </div>
   )
 }
 
-function InputField({ id, value, onChange }) {
+function InputField({ id, label, value, onChange }) {
 
   return (
-      <label>First Name
+      <label>{label}
         <input id={id} value={value} onChange={onChange} />
       </label>
   )
