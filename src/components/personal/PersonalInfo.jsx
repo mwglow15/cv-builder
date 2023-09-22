ca406a2d4b0d23f116175f8bd26790a501972771
Matt Glowacki <mattwglowacki@gmail.com>
2023-09-21 21:44:48 -0400

    Initial layout for personal info

--- src/components/personal/PersonalInfo.jsx
+++ src/components/personal/PersonalInfo.jsx
@@ -1,9 +1,10 @@
 import "../../styles/personalInfo.css"
+import "../../styles/infoSection.css"
 
 export default function PersonalInfo({ personalInfo }) {
   return(
     <div className="info-section personal-info">
-      <div>{personalInfo.firstName} { ' ' } {personalInfo.lastName} </div>
+      <div className="name">{personalInfo.firstName} { ' ' } {personalInfo.lastName} </div>
       <div className="contact-info">
         <div>{personalInfo.email}</div>
         <div>{personalInfo.phone}</div>
